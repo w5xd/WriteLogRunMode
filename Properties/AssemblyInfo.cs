fba15ed3fe9274a2562fef18073602c2d53d99da
Wayne (Round Rock) <w5xd@writelog.com>
2015-12-25 16:14:31 +0000

    update date

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("WriteLog Contesting Software LLC")]
 [assembly: AssemblyProduct("WriteLogRunMode")]
-[assembly: AssemblyCopyright("Copyright © WriteLog Contesting Software LLC 2015")]
+[assembly: AssemblyCopyright("Copyright © WriteLog Contesting Software LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
