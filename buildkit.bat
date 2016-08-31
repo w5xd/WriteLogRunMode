18bceb2362f9d92a42fd9a07a2f9493d67af56f4
Wayne (Round Rock) <w5xd@writelog.com>
2016-08-31 12:46:45 +0000

    automate build

--- buildkit.bat
+++ buildkit.bat
@@ -1,12 +1,16 @@
 @rem dependencies
 @rem    install 7zip and put it on the path
 @rem fetch/install the Install subproject
-@rem
-del WriteLogRunMode.zip
-7z a WriteLogRunMode.zip install.bat uninstall.bat readme.txt
+@echo off
+call GetBuildVer.bat
+setlocal
+set zip=WriteLogRunModeV.2.%_BuildVersion%.zip
+del /q WriteLogRunMode*.zip
+7z a %zip% install.bat uninstall.bat readme.txt
 pushd Install\bin\Release
-7z a ..\..\..\WriteLogRunMode.zip ConfigureShortcuts.exe* CopyShortcutAssembly.exe*
+7z a ..\..\..\%zip% ConfigureShortcuts.exe* CopyShortcutAssembly.exe*
 popd
 pushd bin\Release
-7z a ..\..\WriteLogRunMode.zip WriteLogRunMode.dll
+7z a ..\..\%zip% WriteLogRunMode.dll
 popd
+move /y %zip% DownloadInstaller
